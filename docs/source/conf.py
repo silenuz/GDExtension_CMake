c7edc51bf71232c6e5877be26ba411236db158fa
silenuznowan <horuuendillus@gmail.com>
2026-04-26 11:35:32 -0400

    tried adding support for gdlexer for code blocks

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,6 +5,10 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import sphinx
+import sphinx_rtd_theme
+import sys
+import os
 
 project = 'GDExtensionCMakeGuide'
 copyright = '2026, Silenuz Nowan'
@@ -12,17 +16,52 @@ author = 'Silenuz Nowan'
 release = '0.1'
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+needs_sphinx = "8.1"
 
-extensions = []
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+sys.path.append(os.path.abspath("_extensions"))
+extensions = [
+    "sphinx_tabs.tabs",
+    "sphinxext.opengraph",
+    "gdscript",
+]
+
+# Warning when the Sphinx Tabs extension is used with unknown
+# builders (like the dummy builder) - as it doesn't cause errors,
+# we can ignore this so we still can treat other warnings as errors.
+sphinx_tabs_nowarn = True
+
+# Disable collapsing tabs for codeblocks.
+sphinx_tabs_disable_tab_closing = True
+
+# The master toctree document
+master_doc = "index"
 
 templates_path = ['_templates']
 exclude_patterns = []
-
-
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+# Theme options
+html_theme_options = {
+    # if we have a html_logo below, this shows /only/ the logo with no title text
+    "logo_only": False,
+    # Collapse navigation (False makes it tree-like)
+    "collapse_navigation": False,
+    # Remove version and language picker beneath the title
+    "version_selector": False,
+    "language_selector": False,
+    # Set Flyout menu to attached
+    "flyout_display": "attached",
+}
+
+# Pygments (syntax highlighting) style to use
+pygments_style = "sphinx"
+highlight_language = "gdscript"
